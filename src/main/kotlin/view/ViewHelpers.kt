8d7dc0829f55fb0bc4e1c13e39e364e1faac629d
edvin <es@syse.no>
2016-07-17 11:34:09 +0200

    Added runAsyncWithProgress

--- src/main/kotlin/view/ViewHelpers.kt
+++ src/main/kotlin/view/ViewHelpers.kt
@@ -1,17 +1,13 @@
 package view
 
+import app.Styles.Companion.content
+import app.Styles.Companion.contentWrapper
 import javafx.scene.layout.HBox
 import javafx.scene.layout.Pane
 import javafx.scene.layout.Priority.ALWAYS
-import app.Styles.Companion.content
-import app.Styles.Companion.contentWrapper
-import controller.GitHub
-import javafx.application.Platform
-import javafx.concurrent.Task
-import javafx.scene.Node
-import javafx.scene.control.ProgressIndicator
-import model.Issue
-import tornadofx.*
+import tornadofx.addClass
+import tornadofx.hbox
+import tornadofx.hboxConstraints
 
 /**
  * Add common wrapper elements used in most Views
@@ -24,22 +20,5 @@ fun Pane.contentBox(op: HBox.() -> Unit) {
             hboxConstraints { hGrow = ALWAYS }
             op()
         }
-    }
-}
-
-fun <T> Node.runAsynWithProgress(op: () -> T): Task<T> {
-    val progress = ProgressIndicator()
-    progress.setPrefSize(boundsInParent.width, boundsInParent.height)
-    val children = parent.getChildList()
-    val index = children.indexOf(this)
-    children.add(index, progress)
-    removeFromParent()
-    return task {
-        val result = op()
-        Platform.runLater {
-            children.add(index, this)
-            progress.removeFromParent()
-        }
-        result
     }
 }
